5cddc704b18b44e59dc249944d3d90808941c38b
Anastasija Lagodzinska <a.lagodzinska@outlook.com>
2025-06-21 19:46:27 -0700

    Base for project + main screen

---
+++ components/MainButton.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, Text, Pressable } from "react-native";
+
+function MainButton() {
+  const handlePress = () => {
+    alert("Button Pressed!");
+  };
+
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
+      onPress={handlePress}
+    >
+      <Text style={styles.buttonText}>START WORKOUT</Text>
+    </Pressable>
+  );
+}
+
+export default MainButton;
+
+const styles = StyleSheet.create({
+  button: {
+    width: 250,
+    paddingVertical: 20,
+    backgroundColor: "#00897B",
+    borderRadius: 12,
+    alignItems: "center",
+    shadowColor: "#004D40",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 5,
+    marginTop: 400,
+  },
+  buttonPressed: {
+    backgroundColor: "#00695C",
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "#FAFAFA",
+    fontSize: 18,
+    fontWeight: "600",
+    letterSpacing: 1,
+  },
+});
---
+++ screens/StartScreen.js
@@ -0,0 +1,43 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  ImageBackground,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import MainButton from "../components/MainButton";
+
+function StartScreen() {
+  return (
+    <LinearGradient colors={["#004D40", "#E3F2FD"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("../assets/images/background-main-screen.avif")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <View style={styles.container}>
+          <MainButton />
+        </View>
+      </ImageBackground>
+    </LinearGradient>
+  );
+}
+
+export default StartScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  rootScreen: {
+    flex: 1,
+    width: "100%",
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
