6f0a01fb16245f816b5510bb841c22e1140afdb9
Anastasija Lagodzinska <a.lagodzinska@outlook.com>
2025-06-23 17:29:36 -0700

    Similar style for second page

--- screens/ExerciseTypeScreen.js
+++ screens/ExerciseTypeScreen.js
@@ -1,38 +1,44 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 function ExerciseTypeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Select Exercise Type</Text>
-      <Button
-        title="Cardio"
-        onPress={() =>
-          navigation.navigate("ExerciseDetails", { type: "Cardio" })
-        }
-      />
-      <Button
-        title="Strength"
-        onPress={() =>
-          navigation.navigate("ExerciseDetails", { type: "Strength" })
-        }
-      />
-      <Button
-        title="Flexibility"
-        onPress={() =>
-          navigation.navigate("ExerciseDetails", { type: "Flexibility" })
-        }
-      />
-    </View>
+    <LinearGradient colors={["#004D40", "#E3F2FD"]} style={styles.rootScreen}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Select Exercise Type</Text>
+        <Button
+          title="Strength"
+          onPress={() =>
+            navigation.navigate("ExerciseDetails", { type: "Strength" })
+          }
+        />
+        <Button
+          title="Cardio"
+          onPress={() =>
+            navigation.navigate("ExerciseDetails", { type: "Cardio" })
+          }
+        />
+        <Button
+          title="Flexibility"
+          onPress={() =>
+            navigation.navigate("ExerciseDetails", { type: "Flexibility" })
+          }
+        />
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    width: "100%",
+  },
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#E3F2FD",
   },
   title: {
     fontSize: 24,
